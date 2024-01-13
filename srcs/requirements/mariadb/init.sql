d7c5587eed3d209c660bfedd73d211a622def1e3
Yono1112 <yuma.ohno.sss@gmail.com>
2024-01-13 22:40:32 +0900

    create my own mariadb image

---
+++ srcs/requirements/mariadb/init.sql
@@ -0,0 +1,24 @@
+-- データベースの作成
+CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+
+-- ユーザーの作成
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+
+-- ユーザーへの権限付与
+GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+
+-- 権限のフラッシュ
+FLUSH PRIVILEGES;
+
+-- データベースの使用
+USE ${MYSQL_DATABASE};
+
+-- テーブルの作成
+CREATE TABLE IF NOT EXISTS admin (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+-- admin ユーザーの追加
+INSERT INTO admin (username, password) VALUES ('${MYSQL_ADMIN_NAME}', '${MYSQL_ADMIN_PASSWORD}');
