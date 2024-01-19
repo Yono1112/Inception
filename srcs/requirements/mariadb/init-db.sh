78ca6f1ed64f2a7ca42c5262e0e60a7f62a8b04c
Yono1112 <yuma.ohno.sss@gmail.com>
2024-01-19 16:15:52 +0900

    add wp-config.php in requirements/mariadb and mv wordpress files to tmp

--- srcs/requirements/mariadb/init-db.sh
+++ srcs/requirements/mariadb/init-db.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 # データベースの作成
-mariadb -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
+# mariadb -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
+mariadb -e "CREATE DATABASE IF NOT EXISTS wordpress;"
 
 # ユーザーの作成と権限付与
-mariadb -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mariadb -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
+# mariadb -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+# mariadb -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
+mariadb -e "CREATE USER 'wordpress'@'%' IDENTIFIED BY 'wordpress';"
+mariadb -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'%';"
 
-mariadb -e "CREATE USER '${MYSQL_ADMIN_NAME}'@'%' IDENTIFIED BY '${MYSQL_ADMIN_PASSWORD}';"
-mariadb -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_ADMIN_NAME}'@'%';"
+# mariadb -e "CREATE USER '${MYSQL_ADMIN_NAME}'@'%' IDENTIFIED BY '${MYSQL_ADMIN_PASSWORD}';"
+# mariadb -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_ADMIN_NAME}'@'%';"
 
 # 権限のフラッシュ
 mariadb -e "FLUSH PRIVILEGES;"
