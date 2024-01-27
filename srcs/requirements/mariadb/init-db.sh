f0428c960c96fb7bb82408eb7b2dc5acf355f20d
Yono1112 <yuma.ohno.sss@gmail.com>
2024-01-27 22:39:26 +0900

    access .env environment variables in mariadb's init-db.sh and wordpress's wp-config.php

--- srcs/requirements/mariadb/init-db.sh
+++ srcs/requirements/mariadb/init-db.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 # データベースの作成
-# mariadb -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
-mariadb -e "CREATE DATABASE IF NOT EXISTS wordpress;"
+mariadb -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
+# mariadb -e "CREATE DATABASE IF NOT EXISTS wordpress;"
 
 # ユーザーの作成と権限付与
-# mariadb -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-# mariadb -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
-mariadb -e "CREATE USER 'wordpress'@'%' IDENTIFIED BY 'wordpress';"
-mariadb -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'%';"
-
-# mariadb -e "CREATE USER '${MYSQL_ADMIN_NAME}'@'%' IDENTIFIED BY '${MYSQL_ADMIN_PASSWORD}';"
-# mariadb -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_ADMIN_NAME}'@'%';"
+mariadb -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mariadb -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
+# mariadb -e "CREATE USER 'wordpress'@'%' IDENTIFIED BY 'wordpress';"
+# mariadb -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'%';"
 
 # 権限のフラッシュ
 mariadb -e "FLUSH PRIVILEGES;"
--- srcs/requirements/wordpress/wp-config.php
+++ srcs/requirements/wordpress/wp-config.php
@@ -20,19 +20,23 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', getenv('WORDPRESS_DB_NAME') ?: 'wordpress' );
+define( 'DB_NAME', getenv('WORDPRESS_DB_NAME') );
+// define( 'DB_NAME', getenv('WORDPRESS_DB_NAME') ?: 'wordpress' );
 // define( 'DB_NAME', 'wordpress' );
 
 /** Database username */
-define( 'DB_USER', getenv('WORDPRESS_DB_USER') ?: 'wordpress' );
+define( 'DB_USER', getenv('WORDPRESS_DB_USER') );
+// define( 'DB_USER', getenv('WORDPRESS_DB_USER') ?: 'wordpress' );
 // define( 'DB_USER', 'wordpress' );
 
 /** Database password */
-define( 'DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD') ?: 'wordpress' );
+define( 'DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD') );
+// define( 'DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD') ?: 'wordpress' );
 // define( 'DB_PASSWORD', 'wordpress' );
 
 /** Database hostname */
-define( 'DB_HOST', getenv('WORDPRESS_DB_HOST') ?: 'db:3306' );
+define( 'DB_HOST', getenv('WORDPRESS_DB_HOST') );
+// define( 'DB_HOST', getenv('WORDPRESS_DB_HOST') ?: 'db:3306' );
 // define( 'DB_HOST', 'db:3306' );
 
 /** Database charset to use in creating database tables. */
