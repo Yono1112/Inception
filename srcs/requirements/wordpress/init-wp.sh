23470d3be57b10059fae9891b241dc36ac1da070
Yono1112 <yuma.ohno.sss@gmail.com>
2024-01-28 17:05:51 +0900

    reference wordpress admin user settings from .env

--- srcs/requirements/wordpress/init-wp.sh
+++ srcs/requirements/wordpress/init-wp.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 if ! wp core is-installed --allow-root ; then
-	# wp core download --allow-root --locale=ja
-	wp core install --allow-root --path=/var/www/html --url="yuohno.42.fr" --title="Yuohno Blog" --admin_user="admin" --admin_password="password" --admin_email="admin@example.com" --locale=ja --skip-email
+	wp core install --allow-root --path=/var/www/html \
+	--url="yuohno.42.fr" \
+	--title="Yuohno Blog" \
+	--admin_user="${WORDPRESS_ADMIN_USER}" \
+	--admin_password="${WORDPRESS_ADMIN_PASSWORD}" \
+	--admin_email="${WORDPRESS_ADMIN_EMAIL}" \
+	--locale=ja \
+	--skip-email
 	wp language core install --allow-root ja
 	wp site switch-language --allow-root ja
 	wp option update --allow-root timezone_string 'Asia/Tokyo'
