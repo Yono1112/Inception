42563ec04f98a373bed68687183e37a88c9823f5
Yono1112 <yuma.ohno.sss@gmail.com>
2024-01-28 15:57:35 +0900

    setup wordpress by wp-cli

---
+++ srcs/requirements/wordpress/init-wp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if ! wp core is-installed --allow-root ; then
+	# wp core download --allow-root --locale=ja
+	wp core install --allow-root --path=/var/www/html --url="yuohno.42.fr" --title="Yuohno Blog" --admin_user="admin" --admin_password="password" --admin_email="admin@example.com" --locale=ja --skip-email
+	wp language core install --allow-root ja
+	wp site switch-language --allow-root ja
+fi
+
+exec /usr/sbin/php-fpm -F
