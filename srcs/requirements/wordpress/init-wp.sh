5e0307fe7f885d388f6f5344f3a4d0fa163f8714
Yono1112 <yuma.ohno.sss@gmail.com>
2024-01-28 16:01:56 +0900

    change time zone to Tokyo

--- srcs/requirements/wordpress/init-wp.sh
+++ srcs/requirements/wordpress/init-wp.sh
@@ -5,6 +5,7 @@ if ! wp core is-installed --allow-root ; then
 	wp core install --allow-root --path=/var/www/html --url="yuohno.42.fr" --title="Yuohno Blog" --admin_user="admin" --admin_password="password" --admin_email="admin@example.com" --locale=ja --skip-email
 	wp language core install --allow-root ja
 	wp site switch-language --allow-root ja
+	wp option update --allow-root timezone_string 'Asia/Tokyo'
 fi
 
 exec /usr/sbin/php-fpm -F
